794620adce7b74827f592964d798df9fbc1f231c
rattanawalee25400@gmail.com <rattanawalee25400@gmail.com>
2019-12-11 09:15:24 +0700

    test

---
+++ src/lab1/hello.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Word, I am gif");
+        System.out.println("MIT");
+
+    }//main
+}//cass
